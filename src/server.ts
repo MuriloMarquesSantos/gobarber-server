441c7706bb7a3768b89049db20cd76302c27e2eb
MuriloMarquesSantos <murilommms@gmail.com>
2020-09-07 22:46:57 -0300

    Adding static files route to application

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/index';
+import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
@@ -10,6 +11,7 @@ const port = 3333;
 app.use(routes);
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
